bb9e39a9f75aff5b299d4678ff86db49532577b9
prashant8218427772 <prashant.verma7772@gmail.com>
2025-12-16 23:10:48 +0530

    Overall code

---
+++ child_module/01_resource_group/variable.tf
@@ -0,0 +1,11 @@
+variable "rgs"{
+    description = "rg ki value"
+    type = map(object({
+      name = string
+      location = string
+      managed_by = optional(string,null)
+      tags = optional(string,null)
+      
+
+    }))
+}
